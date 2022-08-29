45bbd2496f1cfa9eac12b6b0d2d201fdd2d68ffd
Tasluf665 <tasluf15-12089@diu.edu.bd>
2022-08-29 15:47:11 +0600

    Customers can upload their profile picture

--- components/ProfileScreenComponent/NonFunctionalComponent/TopPart.js
+++ components/ProfileScreenComponent/NonFunctionalComponent/TopPart.js
@@ -1,20 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Image, TouchableNativeFeedback } from "react-native";
 import { ScaledSheet } from "react-native-size-matters";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 import Colors from "../../../Constant/Colors";
 
 const TopPart = (props) => {
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const result = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (!result.granted) {
+      alert("You need to enable permission to add profile picture");
+    }
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await FileSystem.readDirectoryAsync(
+        FileSystem.documentDirectory
+      );
+
+      if (res.includes("myimg.jpg")) {
+        setImageUri(FileSystem.documentDirectory + "myimg.jpg");
+      }
+    };
+    getData();
+    return () => {
+      setImageUri(FileSystem.documentDirectory + "myimg.jpg");
+    };
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        const newPath = FileSystem.documentDirectory + "myimg.jpg";
+
+        await FileSystem.copyAsync({
+          from: result.uri,
+          to: newPath,
+        });
+
+        setImageUri(result.uri);
+      }
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: TopPart.js ~ line 24 ~ selectImage ~ error",
+        error
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableNativeFeedback
-        onPress={() => {
-          console.log("Touched");
-        }}
-      >
+      <TouchableNativeFeedback onPress={selectImage}>
         <Image
           style={styles.image}
-          source={require("../../../assets/Images/Rhyme.jpg")}
+          source={
+            imageUri
+              ? { uri: imageUri }
+              : require("../../../assets/Images/profile-icon.jpg")
+          }
         />
       </TouchableNativeFeedback>
 
